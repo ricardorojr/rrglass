d7052df8a4e6a9071c47ab49148698579dcdc12f
ricardorojr <ricardorodriguesjr87@gmail.com>
2026-01-11 10:58:34 -0300

    Initial Project

---
+++ src/main/java/com/rrglass/br/RrglassApplication.java
@@ -0,0 +1,13 @@
+package com.rrglass.br;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RrglassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RrglassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rrglass/br/RrglassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rrglass.br;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RrglassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
