c194d162a866dfd5fb931abae6144a78d2fa474c
ricardorojr <ricardorodriguesjr87@gmail.com>
2026-01-11 18:02:55 -0300

    update table users

--- src/main/resources/db/migration/V2601111629__create_user_table.sql
+++ src/main/resources/db/migration/V2601111629__create_user_table.sql
@@ -1,12 +1,12 @@
 CREATE TABLE usuarios (
-    id SERIAL PRIMARY KEY, -- Chave primária autoincremental
-    nome VARCHAR(100) NOT NULL, -- Nome completo do usuário (obrigatório)
-    email VARCHAR(255) UNIQUE NOT NULL, -- Email (único e obrigatório)
-    senha_hash VARCHAR(255) NOT NULL, -- Armazena o hash da senha (nunca a senha em texto plano!)
-    cpf VARCHAR(11) UNIQUE, -- CPF (opcional, mas único se preenchido)
-    data_nascimento DATE, -- Data de nascimento (opcional)
-    telefone VARCHAR(20), -- Telefone (opcional)
-    ativo BOOLEAN DEFAULT TRUE, -- Status do usuário (ativo/inativo), padrão TRUE
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    senha_hash VARCHAR(255) NOT NULL,
+    cpf VARCHAR(11) UNIQUE,
+    data_nascimento DATE,
+    telefone VARCHAR(20),
+    ativo BOOLEAN DEFAULT TRUE,
     data_criacao TIMESTAMPTZ,
     data_atualizacao TIMESTAMPTZ
 );
